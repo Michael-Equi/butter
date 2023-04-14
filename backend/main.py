0396c8da928f385d705c0f0351f921d87565d544
Michael Lutz <michaeljeffreylutz@gmail.com>
2023-04-13 23:39:18 -0700

    initial push

--- backend/main.py
+++ backend/main.py
@@ -41,6 +41,7 @@ def run_analytics_handler():
     running_expected_sentiment = 0
     running_test_sentiment = 0
     running_counter = 0
+    running_correctness = 0
 
     for test in tests:
         """title = test['title']
@@ -52,16 +53,19 @@ def run_analytics_handler():
         test_jaccard_similarity = 0
         test_expected_sentiment = 0
         test_test_sentiment = 0
+        test_correctness = 0
         test_counter = 0
         
         print(running_counter)
         for case in test['cases']:
             outputs = case['outputs']
             expected = case['expected']
+            inputs = case['input']
             semantic_similarities = []
             jaccard_similarities = []
             expected_sentiments = []
             test_sentiments = []
+            gpt3_correctness = []
             # Calculating the similarity between the expected output and the test output
             #for i in range(len(expected)):
             #try:
@@ -83,9 +87,11 @@ def run_analytics_handler():
                 test_sentiments.append(_get_sentiment(outputs[-1]))
             except:
                 test_sentiments.append(0)
-            #except Exception as e:
-            #    print('An error occurred:', e)
-            # Average similarity
+            try:
+                gpt3_correctness.append(_gpt3_correctness(inputs[-1], outputs[-1], expected))
+            except:
+                gpt3_correctness.append(0)
+
             average_semantic_similarity = np.mean(semantic_similarities)
             average_jaccard_similarity = np.mean(jaccard_similarities)
             average_expected_sentiment = np.mean(expected_sentiments)
@@ -95,11 +101,17 @@ def run_analytics_handler():
             case['jaccardSimilarity'] = average_jaccard_similarity
             case['expectedSentiment'] = average_expected_sentiment
             case['testSentiment'] = average_test_sentiment
+            case['gptCorrectness'] = gpt3_correctness
             
             running_semantic_similarity += average_semantic_similarity
             running_jaccard_similarity += average_jaccard_similarity
             running_expected_sentiment += average_expected_sentiment
             running_test_sentiment += average_test_sentiment
+
+            if gpt3_correctness == "CORRECT":
+                test_correctness += 1
+                running_correctness += 1
+
             running_counter += 1
 
             test_semantic_similarity += average_semantic_similarity
@@ -112,6 +124,7 @@ def run_analytics_handler():
         test['jaccardSimilarity'] = test_jaccard_similarity / test_counter
         test['expectedSentiment'] = test_expected_sentiment / test_counter
         test['testSentiment'] = test_test_sentiment / test_counter
+        test['testCorrectness'] = test_correctness / test_counter
 
         test['createdAt'] = datetime.datetime.utcnow()
         test['updatedAt'] = datetime.datetime.utcnow()
@@ -123,6 +136,7 @@ def run_analytics_handler():
     content['averageJaccardSimilarity'] = running_jaccard_similarity / running_counter
     content['averageExpectedSentiment'] = running_expected_sentiment / running_counter
     content['averageTestSentiment'] = running_test_sentiment / running_counter
+    content['averageCorrectness'] = running_correctness / running_counter
     content['projectId'] = ObjectId(content['projectId'])
     content['name'] = randomname.get_name()
     content['_id'] = test_run_id
@@ -198,5 +212,41 @@ def _jaccard_similarity(string1, string2):
 
 
 
+def _gpt3_correctness(question, response, expected):
+    prompt = """You are a teacher grading a quiz.
+    You are given a question, the student's answer, and the true answer, and are asked to score the student answer as either CORRECT or INCORRECT.
+    Example Format:
+    QUESTION: question here
+    STUDENT ANSWER: student's answer here
+    TRUE ANSWER: true answer here
+    GRADE: CORRECT or INCORRECT here
+    Grade the student answers based ONLY on their factual accuracy. Ignore differences in punctuation and phrasing between the student answer and true answer. It is OK if the student answer contains more information than the true answer, as long as it does not contain any conflicting statements. Begin! 
+    QUESTION: {question}
+    STUDENT ANSWER: {response}
+    TRUE ANSWER: {expected}
+    GRADE:"""
+
+    # Create the final prompt with the values
+    final_prompt = prompt.format(question=question, response=response, expected=expected)
+
+    """response = openai.Completion.create(
+        engine="gpt-3.5-turbo",
+        prompt=final_prompt,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )"""
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": final_prompt}])
+    
+
+    return response.choices[0].message.content
+
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=3001))
+    #app.run(debug=True, port=os.getenv("PORT", default=3001))
+
+    print(_gpt3_correctness("What is the capital of France?", "Paris", "Paris"))
